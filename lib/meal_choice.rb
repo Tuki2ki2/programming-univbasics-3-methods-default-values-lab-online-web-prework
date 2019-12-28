523cd9ca3000f66891d422f190b20180644f0fcc
Erik Prenzlin <eprenzlin@gmail.com>
2019-12-28 14:50:03 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,7 @@
 
 def meal_choice (veg1,veg2,protein = "meat")
   puts " What a nutritious meal!" 
-  puts " A place of #{protein}, with #{veg1} and #{veg2}."
+  meal_confirm= " A plate of #{protein}, with #{veg1} and #{veg2}."
+  puts meal_confirm
 end 
 
